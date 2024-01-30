506759787b1977bfd711a2aa3ef7140295a8b281
Ahsn@1234 <ahmad507eb@gmail.com>
2024-01-30 10:39:07 +0500

    creating homepage(Ecommerce)

--- src/App.js
+++ src/App.js
@@ -14,16 +14,16 @@ const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <div className="flex relative dark:bg-main-dark-bg">
+        <div className="relative flex dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{zindex: '1000'}}>
             <TooltipComponent content="Settings" position="Top">
-              <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{background: 'blue', borderRadius: '50%'}}>
+              <button type="button" className="p-3 text-3xl text-white hover:drop-shadow-xl hover:bg-light-gray" style={{background: 'blue', borderRadius: '50%'}}>
                 <FiSettings/>
               </button>
             </TooltipComponent>
           </div>
           {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+            <div className="fixed bg-white w-72 sidebar dark:bg-secondary-dark-bg">
               <Sidebar/>
             </div>
           ): (
@@ -35,10 +35,9 @@ const App = () => {
             `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'
             }`
           }>
-            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+            <div className="fixed w-full md:static bg-main-bg dark:bg-main-dark-bg navbar">
                 <Navbar/>
             </div>
-          </div>
           <div>
             <Routes>
               {/* Dashboard */}
@@ -68,6 +67,7 @@ const App = () => {
 
             </Routes>
           </div>
+        </div>
         </div>
       </BrowserRouter>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,7 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
   const {activeMenu, setactiveMenu, screenSize} = useStateContext();
-  const activeLink="flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+  const activeLink="flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-700 text-md m-2"; // when dark mode implement change text-color of this class to text-white
   const normalLink="flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2 ";
 
   const handleCloseSidebar=()=>{
@@ -20,11 +20,11 @@ const Sidebar = () => {
   }
 
   return (
-    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+    <div className="h-screen pb-10 ml-3 overflow-auto md:overflow-hidden md:hover:overflow-auto">
       {activeMenu && (
         <>
-        <div className="flex justify-between items-center">
-          <Link to="/" onClick={handleCloseSidebar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+        <div className="flex items-center justify-between">
+          <Link to="/" onClick={handleCloseSidebar} className="flex items-center gap-3 mt-4 ml-3 text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
             <SiShopware/> <span>Shoppy</span>
           </Link>
           <TooltipComponent content="Menu" position="BottomCenter">
@@ -32,7 +32,7 @@ const Sidebar = () => {
               (prevActiveMenu)=>
                 !prevActiveMenu
               )}
-              className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden">
+              className="block p-3 mt-4 text-xl rounded-full hover:bg-light-gray md:hidden">
               <MdOutlineCancel/>
             </button>
           </TooltipComponent>
@@ -40,7 +40,7 @@ const Sidebar = () => {
         <div className="mt-10">
           {links.map((item)=>(
             <div key={item.title}>
-              <p className="text-gray-400 m-3 mt-4 uppercase">
+              <p className="m-3 mt-4 text-gray-400 uppercase">
               {item.title}
               </p>
               {item.links.map((link)=>(
