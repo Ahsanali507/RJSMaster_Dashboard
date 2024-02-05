74470a80837136cd060817b17ee5da85e58c6a03
Ahsn@1234 <ahmad507eb@gmail.com>
2024-02-05 14:22:26 +0500

    additions

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -4,7 +4,11 @@ const Editor = () => {
   return (
     <div>
       <div>File</div>
-      
+      <div>Home</div>
+      <div>Insert</div>
+      <div>Design</div>
+      <div>View</div>
+      <div>Layout</div>
     </div>
   )
 }
