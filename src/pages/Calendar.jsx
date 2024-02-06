cc5bf28913efe8ce8efabdf797d34703ee7b93c3
Ahsn@1234 <ahmad507eb@gmail.com>
2024-02-06 15:10:37 +0500

    create calandar

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-
+import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule'
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars'
+import { scheduleData } from '../data/dummy'
+import { Header } from '../components'
 const Calendar = () => {
   return (
-    <div>
-      <form action="">
-        <div>
-          <input type="text" placeholder='Enter your name'/>
-          <input type="date"/>
-        </div>
-      </form>
+    <div className='p-2 m-2 mt-24 bg-white md:m-10 md:p-10 rounded-3xl'>
+      <Header category="App" title="Calendar"/>
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{dataSource: scheduleData}}
+        selectedDate={new Date(2024, 6, 3)}
+      >
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}/>
+      </ScheduleComponent>
     </div>
   )
 }
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,31 +1,9 @@
 import React from 'react'
 
 const Orders = () => {
-  const orders=[{
-    id:1,
-    itemType:"Fruits",
-    quantity:12
-  },{
-    id:2,
-    itemType:"Vegetables",
-    quantity:14
-  },{
-    id:3,
-    itemType:"Others",
-    quantity:20
-  }]
   return (
     <div>
-      {
-        orders.map((item)=>(
-          <div key={item.id}>
-            {/* <orderItem id={item.id} itemType={item.itemType} quantity={item.quantity}></orderItem> */}
-            <h4>Item ID: {item.id}</h4>
-            <h4>ItemType: {item.itemType}</h4>
-            <h4>Item Quantity: {item.quantity}</h4>
-          </div>
-        ))
-      }
+      
     </div>
   )
 }
