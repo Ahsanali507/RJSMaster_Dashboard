98b6255ae73202f9b49ef2fade2b57548ce4a0ec
Ahsn@1234 <ahmad507eb@gmail.com>
2024-02-29 17:05:47 +0500

    cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,83 +1,81 @@
-import React, { useState, useEffect, useRef } from 'react';
-// import { DialogComponent, CheckBoxComponent } from '@syncfusion/ej2-react-popups';
-import { DialogComponent } from '@syncfusion/ej2-react-popups';
-import { ChangeEventArgs } from '@syncfusion/ej2-buttons';
-// import { PropertyPane } from '../common/property-pane';
-// import './modal-dialog.css';
+import { Fragment, useRef, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 
-const Cart = () => {
-    let dialogInstance = useRef(null);
-    let animationSettings;
-    let buttons;
-    let buttonEle;
-    const [display, setDisplay] = useState('none');
-    const [status, setStatus] = useState(true);
+const Cart=()=> {
+  const [open, setOpen] = useState(true)
 
-    animationSettings = { effect: 'None' };
-    buttons = [
-        {
-            click: () => {
-                setStatus(false);
-            },
-            buttonModel: {
-                isPrimary: true,
-                content: 'OK',
-            },
-        },
-    ];
+  const cancelButtonRef = useRef(null)
 
-    const onChange = (args) => {
-        if (args.checked) {
-            dialogInstance.current.overlayClick = () => {
-                setStatus(false);
-            };
-        } else {
-            dialogInstance.current.overlayClick = () => {
-                setStatus(true);
-            };
-        }
-    }
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
+        </Transition.Child>
 
-    const buttonClick = () => {
-        setStatus(true);
-    }
-
-    const dialogClose = () => {
-        setStatus(false);
-        setDisplay('inline-block');
-    }
-
-    const dialogOpen = () => {
-        setStatus(true);
-        setDisplay('none');
-    }
-
-    return (
-        <div className="control-pane">
-            <div className="control-section modal-dialog-target">
-                <div id="target" className="col-lg-8">
-                    <button className="e-control e-btn dlgbtn dlgbtn-position" ref={(element) => buttonEle = element} onClick={buttonClick} style={{ display: display }}>Open</button>
-                    <DialogComponent id="modalDialog" isModal={true} buttons={buttons} header="Software Update" width="335px" content="Your current software version is up to date." ref={dialogInstance} target="#target" visible={status} open={dialogOpen} close={dialogClose} animationSettings={animationSettings}></DialogComponent>
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex items-end justify-center min-h-full p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationTriangleIcon className="w-6 h-6 text-red-600" aria-hidden="true" />
+                    </div>
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                        Deactivate account
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Are you sure you want to deactivate your account? All of your data will be permanently
+                          removed. This action cannot be undone.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className="col-lg-4 property-section">
-                    {/* <PropertyPane title="Properties"> */}
-                        <table id="property" title="Properties" className="property-panel-table table-width">
-                            <tbody>
-                                <tr>
-                                    <td className="table-td">
-                                        <div className="dialog-td-font">Close on overlay click</div>
-                                    </td>
-                                    {/* <td>
-                                        <CheckBoxComponent checked={false} change={onChange}></CheckBoxComponent>
-                                    </td> */}
-                                </tr>
-                            </tbody>
-                        </table>
-                    {/* </PropertyPane> */}
+                <div className="px-4 py-3 bg-gray-50 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center w-full px-3 py-2 text-sm font-semibold text-white bg-red-600 rounded-md shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                    onClick={() => setOpen(false)}
+                  >
+                    Deactivate
+                  </button>
+                  <button
+                    type="button"
+                    className="inline-flex justify-center w-full px-3 py-2 mt-3 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
                 </div>
-            </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
         </div>
-    );
+      </Dialog>
+    </Transition.Root>
+  )
 }
 
 export default Cart;
